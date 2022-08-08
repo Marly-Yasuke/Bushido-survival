8d50fc1024d1829c9c2a33e787d4d12190db368e
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-08-08 16:26:43 +0200

    structure clean, keyboard setup, js error fixed

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,12 @@ const gridElement = document.querySelector('.grid')
 const gridColumns = 10;
 const gridRows = 10;
 const cells = [];
+let score = 0;
 
 function startNewGame() {
   createTheGrid();
+  getRandomSelection();
+  distributeCollectibles()
 }
 const startButton = document.getElementById('new-game')
 startButton.addEventListener('click', () => {
@@ -23,3 +26,40 @@ function createTheGrid() {
   }
 }
 
+// I create an array of item that will be collectibles
+const collectibles = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+]
+
+// DistributeCollectibles function will randomly display collectibles when launching the game
+
+// Keyboard setup
+document.addEventListener('keydown', (event) => {
+  if (!game.isStarted) {
+    return
+  }
+
+  switch (event.code) {
+    case 'ArrowUp':
+      player.move('up')
+      break
+    case 'ArrowDown':
+      player.move('down')
+      break
+    case 'ArrowLeft':
+      player.move('left')
+      break
+    case 'ArrowRight':
+      player.move('right')
+      break
+  }
+})
